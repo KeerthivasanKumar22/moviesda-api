f1e57e3c14010e2da4eff42c280a5f43aae67b08
KumarKeerthivasan <141889131+KumarKeerthivasan@users.noreply.github.com>
2024-12-07 18:00:21 +0530

    route structure

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ const PORT = 8000
 
 app.get('/', (req,res)=>{
     res.json({msg:"Hello Makkalay"})
+});
+
+// CRUD functionality of movies
+
+
+
+// R - For Reading movie
+app.get('/movies', () =>{
+
+})
+
+// C - For Creating movie
+app.post('/movies', () =>{
+    
+})
+
+// U - For Update movie
+app.put('/movies', () =>{
+    
+})
+
+// D - For Deleting movie
+app.delete('/movies', () =>{
+    
 })
 
 
